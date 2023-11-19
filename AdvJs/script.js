daa090bac7a4f68ba8adcd27d07e6ea5a196313d
Chandu_Pingili <126405951+ChanduPingili@users.noreply.github.com>
2023-11-19 21:56:54 +0530

    Add files via upload

---
+++ AdvJs/script.js
@@ -0,0 +1,52 @@
+
+let person={
+  name:"Chandu",
+  age:20
+};
+
+let employee={
+  role:"Developer",
+  __proto__:person
+};
+//colsure
+function calAge(born,present){
+  function calc(){
+          return present-born;
+      };
+  return calc();
+}
+
+//callbacks
+function validAge(age){
+  return age>=20;
+}
+
+function age(age, callback){
+  console.log(`Employee is set for job : ${callback(age)}`)
+}
+
+//promises
+async function salInc(sal,exp){
+  let val= true;
+  if(exp >= 5)
+      val = false;
+  await checkExp(sal,val).then(function(result){
+      console.log(result);
+      return result;
+  }).catch(function(result){
+      console.log(result)});
+}
+
+function checkExp(value,error){
+  return new Promise((resolve,reject) =>{
+      if(!error){
+          resolve(`Salary incremented from ${value} to ${value+value/10} as he has more than 5 years of experience`);
+      }else
+          reject("Salary can't be increased due to lack of experience");
+  });
+};
+
+console.log(`Employee Name : ${employee.name}`)
+console.log(`Employee age : ${calAge(2003,2023)}`);
+age(employee.age, validAge)
+salInc(10000,5);
